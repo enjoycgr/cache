2015a235e0a85335971450b51b85f936ee00ca50
guorong <464884015@qq.com>
2021-12-10 18:40:17 +0800

    rpc

---
+++ rpc/codec/codec.go
@@ -0,0 +1,31 @@
+package codec
+
+import "io"
+
+type Header struct {
+	ServiceMethod string // 服务名和方法名："Service.Method"
+	Seq           uint64 // 请求编号
+	Error         string
+}
+
+type Codec interface {
+	io.Closer
+	ReadHeader(*Header) error
+	ReadBody(interface{}) error
+	Write(*Header, interface{}) error
+}
+
+type NewCodecFunc func(closer io.ReadWriteCloser) Codec
+
+type Type string
+
+const (
+	GobType Type = "application/gob"
+)
+
+var NewCodecFuncMap map[Type]NewCodecFunc
+
+func init() {
+	NewCodecFuncMap = make(map[Type]NewCodecFunc)
+	NewCodecFuncMap[GobType] = NewGobCodec
+}
---
+++ rpc/server/server.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"cache/rpc/codec"
+	"encoding/json"
+	"io"
+	"log"
+	"net"
+)
+
+// | Option{MagicNumber: xxx, CodecType: xxx} | Header{ServiceMethod ...} | Body interface{} |
+// | <------      固定 JSON 编码      ------>  | <-------   编码方式由 CodeType 决定   ------->|
+
+const MagicNumber = 0x3bef5c
+
+type Option struct {
+	MagicNumber int
+	CodecType   codec.Type
+}
+
+var DefaultOption = &Option{
+	MagicNumber: MagicNumber,
+	CodecType:   codec.GobType,
+}
+
+type Server struct{}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+var DefaultServer = NewServer()
+
+func (server *Server) Accept(lis net.Listener) {
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println("rpc server: accept error:", err)
+			return
+		}
+		go server.ServeConn(conn)
+	}
+}
+
+func Accept(lis net.Listener) {
+	DefaultServer.Accept(lis)
+}
+
+func (server *Server) ServeConn(conn io.ReadWriteCloser) {
+	defer func() {
+		_ = conn.Close()
+	}()
+
+	var opt Option
+	if err := json.NewDecoder(conn).Decode(&opt); err != nil {
+		log.Println("rpc server: options error: ", err)
+		return
+	}
+}
