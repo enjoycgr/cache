6e33ee9227a47b001c403326ca1cc4448029426d
guorong <464884015@qq.com>
2022-01-10 19:05:09 +0800

    benchmark

---
+++ benchmark_test.go
@@ -0,0 +1,27 @@
+package cache
+
+import (
+	"cache/rpc"
+	"cache/server"
+	"context"
+	"log"
+	"strconv"
+	"testing"
+)
+
+func BenchmarkSet(b *testing.B) {
+	client, err := rpc.Dial("tcp", ":8001")
+	if err != nil {
+		log.Fatalln("network dial error: ", err)
+	}
+	args := server.SetRequest{
+		Key:   strconv.Itoa(1),
+		Value: strconv.Itoa(1),
+	}
+	var reply server.SetResponse
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		client.Call(context.Background(), "Server.Set", args, &reply)
+	}
+}
